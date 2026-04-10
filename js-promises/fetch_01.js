fef4b9c94932751be7a924bd4f4d7ba22bb8c5f6
Nattagorn <jay@MacBook-Air-khxng-Nattagorn.local>
2026-04-10 10:23:20 +0700

    added-fetch02

--- js-promises/fetch_01.js
+++ js-promises/fetch_01.js
@@ -1,3 +1,7 @@
+const url = "https://jsonplaceholder.typicode.com/posts/1"
+
+fetch(url);
+
 fetch("https://jsonplaceholder.typicode.com/posts/1")
 .then((response) =>{
     return response.json();
@@ -5,6 +9,8 @@ fetch("https://jsonplaceholder.typicode.com/posts/1")
 .then((data) => {
     console.log(data);
 })
+
+// handle error gracefully 
 .catch((error) => {
     console.log("Something went wrong!", error)
 });
